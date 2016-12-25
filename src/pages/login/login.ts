e3ac162724ae78d9cc03e71f9260b25e9e3a2a6a
FazioNico <contact@nicolasfazio.ch>
2016-12-26 00:10:27 +0100

    upd header info

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,3 +1,11 @@
+/**
+* @Author: Nicolas Fazio <webmaster-fazio>
+* @Date:   25-12-2016
+* @Email:  contact@nicolasfazio.ch
+* @Last modified by:   webmaster-fazio
+* @Last modified time: 26-12-2016
+*/
+
 import { Component } from '@angular/core';
 import { NavController, LoadingController, AlertController } from 'ionic-angular';
 import {Validators, FormBuilder } from '@angular/forms';
